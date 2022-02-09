9448f7c51694f2d5516b8fb3ce3120db86c10bb5
alexzhli <alexzhli@tencent.com>
2022-02-09 10:14:17 +0800

    add: add frontend dist

---
+++ client/dist/js/___vite-browser-external_commonjs-proxy-604003e4.js
@@ -0,0 +1,2 @@
+import{a2 as o}from"./vendor-0406d426.js";var r=o(Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:{}}));export{r};
+//# sourceMappingURL=___vite-browser-external_commonjs-proxy-604003e4.js.map
